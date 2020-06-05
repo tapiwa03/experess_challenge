bd8e75cf61f01dfb85bc21d4fe60062c97fb45fa
tapiwa03 <mapfundetl@gmail.com>
2020-06-05 16:49:49 +0200

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import { indexRoute } from './routes'
-import { generateRandomUsers } from './utils'
 
 const app = express()
 app.use(cors())
 app.use('', indexRoute)
-generateRandomUsers()
 
-export default app
+export { app}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import app from './app'
+import { app}  from './app'
 
-app.listen(3030, () => { console.log('Server starting...') })
+app.listen(3030, () => { 
+  console.log('Server starting on port 3030...') 
+})
